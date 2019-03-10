983d09b02d80bcce3d9c9fe9f8e3b1ee8847580f
Morgan Galvin <morgan@ludwigthepig.com>
2019-03-11 09:04:12 +1300

    Coordinated click events
    clicking see new tweets updates the update state and button display

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ $(document).ready(function(){
     let lastUpdate = streams.home.length;
     let update = true;
 
+    const renderTweets= (array)=>{
+      for (let i = 0; i<array.length; i++){
+        const tweet = array[i];
+        const $tweet = $('<div></div>');
+        const $leftCol = $("<div></div>")
+        $leftCol.append("<img class='egg' src='egg.jpg'></img>")
+        $leftCol.append(`<span class='user'>@${tweet.user}:</span>`)
+        $leftCol.append(`<p class="message">${tweet.message}</p>`);
+        $tweet.append($leftCol);
+        $tweet.append(`<span class="date">${timeAgo(tweet.created_at)}</span>`);
+        $tweet.addClass('tweetDiv');
+        $leftCol.addClass('leftCol');
+        $tweet.click(()=>{renderUserStream(streams.users[tweet.user])});
+        
+        $tweet.prependTo($body);
+      }
+    }
+
     //Takes the date and returns how long ago that date was
     const timeAgo = (date)=>{
         var seconds = Math.floor((new Date() - date) / 1000);
@@ -31,24 +49,6 @@ $(document).ready(function(){
         return Math.floor(seconds) + " seconds ago";    
     }
 
-    const renderTweets= (array)=>{
-      for (let i = 0; i<array.length; i++){
-        const tweet = array[i];
-        const $tweet = $('<div></div>');
-        const $leftCol = $("<div></div>")
-        $leftCol.append("<img class='egg' src='egg.jpg'></img>")
-        $leftCol.append(`<span class='user'>@${tweet.user}:</span>`)
-        $leftCol.append(`<p class="message">${tweet.message}</p>`);
-        $tweet.append($leftCol);
-        $tweet.append(`<span class="date">${timeAgo(tweet.created_at)}</span>`);
-        $tweet.addClass('tweetDiv');
-        $leftCol.addClass('leftCol');
-        $tweet.click(()=>{renderUserStream(streams.users[tweet.user])});
-        
-        $tweet.prependTo($body);
-      }
-    }
-
     const renderUserStream = (user)=>{
       $body.html('')
       update = false;
@@ -73,12 +73,30 @@ $(document).ready(function(){
         lastUpdate += currTweets.length - lastUpdate;
       }
     }
+    const displayUpdate = ()=>{
+        let currTweets = streams.home;
+        if(currTweets.length > lastUpdate){
+            let newTweets = currTweets.slice(lastUpdate);
+            if (newTweets.length > 1){
+                $("#update").html(`<span class='newTweets'>See ${newTweets.length} new Tweets</span>`)
+            } else {
+                $("#update").html(`<span class='newTweets'>See ${newTweets.length} new Tweet</span>`)
+            }
+            $(".newTweets").click(()=>{
+                update = !update;
+                $("#update").html('');
+                $("#updateToggle").text("Stop Stream");
+            })
+        }
+    }
 
     //initial function calls
     renderTweets(streams.home);
     setInterval(()=>{
       if(update === true){
         getNewTweets();
+      } else {
+          displayUpdate();
       }
     }, 1500);         
   });
