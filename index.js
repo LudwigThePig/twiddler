28d6ad3ad74a0f89675cc957daefcf344bae2a62
Morgan Galvin <morgan@ludwigthepig.com>
2019-03-12 08:12:36 +1300

    Added randomSVGGeneartor to the user's object
    Render newTweets will pull that svg from the users object and render it

--- index.js
+++ index.js
@@ -5,13 +5,31 @@ $(document).ready(function(){
     let lastUpdate = streams.home.length;
     let update = true;
 
+    //This creates a random SVG for the user's profile picture
+    const randomSvgGen = ()=>{
+      const colors = [['#59FAC1','#FFBD5B'],['#FF9508', '#FF0814'],['#102467', '#870730'],['#00FFBF', '#FF0063'],['#FFBD5B', '#59FAC1'], ['#FF0814', '#FF9508'],['#870730', '#102467'],['#FF0063', '#00FFBF']];
+      const randomNum = Math.floor(Math.random() * 8);
+      const col = {
+        background: colors[randomNum][0],
+        fill: colors[randomNum][1]
+      }  
+      return `<svg height="50" width="50" style="background: ${col.background}">
+        <circle cx="25" cy="25" r="20" fill="${col.fill}" />
+        </svg> `;
+    }
+    //this assigns a randomSVG to the user object
+    Object.keys(streams.users).forEach( i => {
+      streams.users[i].svg = randomSvgGen();
+    })
+
     const renderTweets= (array)=>{
       $body.html('');
       for (let i = 0; i<array.length; i++){
         const tweet = array[i];
+        const user = array[i].user;
         const $tweet = $('<div></div>');
         const $leftCol = $("<div></div>");
-        $leftCol.append(randomSvgGen());
+        $leftCol.append(streams.users[user].svg);
         $leftCol.append(`<span class='user'>@${tweet.user}:</span>`);
         $leftCol.append(`<p class="message">${tweet.message}</p>`);
         $tweet.append($leftCol);
@@ -70,7 +88,7 @@ $(document).ready(function(){
       let currTweets = streams.home;
       if(currTweets.length > lastUpdate){
         let newTweets = currTweets.slice(lastUpdate);
-        renderTweets(currTweets);
+        renderTweets(newTweets);
         lastUpdate += currTweets.length - lastUpdate;
       }
     }
@@ -90,20 +108,6 @@ $(document).ready(function(){
               $("#update").html('');
             })
         }
-    }
-
-    const randomSvgGen = ()=>{
-      const colors = [['#59FAC1','#FFBD5B'],['#FF9508', '#FF0814'],['#102467', '#870730'],['#00FFBF', '#FF0063'],
-      ['#FFBD5B', '#59FAC1'], ['#FF0814', '#FF9508'],['#870730', '#102467'],['#FF0063', '#00FFBF']];
-      const randomNum = Math.floor(Math.random() * 8);
-      const col = {
-        background: colors[randomNum][0],
-        fill: colors[randomNum][1]
-      }  
-      const svg = `<svg height="50" width="50" style="background: ${col.background}">
-        <circle cx="25" cy="25" r="20" fill="${col.fill}" />
-        </svg> `;
-      return svg;
     }
 
     //initial function calls
