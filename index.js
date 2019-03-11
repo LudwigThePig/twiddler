8c0f6a9157cd6801b8e31a6875ef26a24f516aa5
Morgan Galvin <morgan@ludwigthepig.com>
2019-03-12 07:43:39 +1300

    Added random svg generator

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ $(document).ready(function(){
     let update = true;
 
     const renderTweets= (array)=>{
+      $body.html('');
       for (let i = 0; i<array.length; i++){
         const tweet = array[i];
         const $tweet = $('<div></div>');
-        const $leftCol = $("<div></div>")
-        $leftCol.append("<img class='egg' src='egg.jpg'></img>")
-        $leftCol.append(`<span class='user'>@${tweet.user}:</span>`)
+        const $leftCol = $("<div></div>");
+        $leftCol.append(randomSvgGen());
+        $leftCol.append(`<span class='user'>@${tweet.user}:</span>`);
         $leftCol.append(`<p class="message">${tweet.message}</p>`);
         $tweet.append($leftCol);
         $tweet.append(`<span class="date">${timeAgo(tweet.created_at)}</span>`);
@@ -69,10 +70,12 @@ $(document).ready(function(){
       let currTweets = streams.home;
       if(currTweets.length > lastUpdate){
         let newTweets = currTweets.slice(lastUpdate);
-        renderTweets(newTweets);
+        renderTweets(currTweets);
         lastUpdate += currTweets.length - lastUpdate;
       }
     }
+
+    //Renders and adds the event listener to the #update section
     const displayUpdate = ()=>{
         let currTweets = streams.home;
         if(currTweets.length > lastUpdate){
@@ -83,11 +86,24 @@ $(document).ready(function(){
                 $("#update").html(`<span class='newTweets'>See ${newTweets.length} new Tweet</span>`)
             }
             $(".newTweets").click(()=>{
-                update = !update;
-                $("#update").html('');
-                $("#updateToggle").text("Stop Stream");
+              getNewTweets();
+              $("#update").html('');
             })
         }
+    }
+
+    const randomSvgGen = ()=>{
+      const colors = [['#59FAC1','#FFBD5B'],['#FF9508', '#FF0814'],['#102467', '#870730'],['#00FFBF', '#FF0063'],
+      ['#FFBD5B', '#59FAC1'], ['#FF0814', '#FF9508'],['#870730', '#102467'],['#FF0063', '#00FFBF']];
+      const randomNum = Math.floor(Math.random() * 8);
+      const col = {
+        background: colors[randomNum][0],
+        fill: colors[randomNum][1]
+      }  
+      const svg = `<svg height="50" width="50" style="background: ${col.background}">
+        <circle cx="25" cy="25" r="20" fill="${col.fill}" />
+        </svg> `;
+      return svg;
     }
 
     //initial function calls
@@ -96,7 +112,7 @@ $(document).ready(function(){
       if(update === true){
         getNewTweets();
       } else {
-          displayUpdate();
+        displayUpdate();
       }
     }, 1500);         
   });
